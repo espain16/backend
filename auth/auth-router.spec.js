5dc5816c4fa9804ea3c436a6795728db1d8f24bc
Elise Spain <elisespain@gmail.com>
2019-08-26 20:13:32 -0700

    Todo helper functions

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,9 @@
+const db = require("../data/dbConfig");
+const request = require("supertest");
+const user = require("./auth-model");
+const server = require("./auth-router");
+
+//test endpoints 
+//register - testing 
+//login test status codes 
+
